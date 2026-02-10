ed82a7f10a21460655f6d4cfc04abe6a15b7ab71
Malik Hussein <malikhussein229@gmail.com>
2026-02-10 22:05:31 +0200

    fix: remove the redirect in the navbar

--- app/_component/navBar.tsx
+++ app/_component/navBar.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import Link from "next/link";
-import { auth } from "@/auth";
-import { redirect } from "next/navigation";
-import { log } from "node:console";
+import React from 'react';
+import Link from 'next/link';
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
+import { log } from 'node:console';
 
 export default async function NavBar() {
   const session = await auth();
@@ -10,24 +10,18 @@ export default async function NavBar() {
 
   console.log(user);
 
-  if (!session) {
-    redirect("/login");
-  }
-
   console.log(session);
 
   return (
     <div className="bg-primary/90 o h-14  text-white">
       <ul className="flex justify-center items-center gap-8 h-full font-medium text-xl">
-      {
-        user === "admin" || user === "moderator" ? (
+        {user === 'admin' || user === 'moderator' ? (
           <Link href="/dashboard"> لوحة التحكم</Link>
-        ) : null
-      }
+        ) : null}
         <Link href="/about">من نحن</Link>
         <Link href="/called">القاب وعزاوي ال العبد</Link>
         <Link href="/posts">مجتمع القبيلـة </Link>
-        <Link href="/">الرئيسية</Link>{" "}
+        <Link href="/">الرئيسية</Link>{' '}
       </ul>
     </div>
   );
