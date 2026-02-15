5b7050d3693ca349b04c04facfd2934d09beb1c7
Malik Hussein <malikhussein229@gmail.com>
2026-02-16 00:05:30 +0200

    feat: add GET endpoint for retrieving family data

---
+++ app/api/family-data/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+import { FamilyData } from '../../../entities/family-data.entity';
+import { getDb } from '../../../lib/db';
+
+export async function GET() {
+  const db = await getDb();
+
+  const familyDataRepo = db.getRepository(FamilyData);
+
+  const familyData = await familyDataRepo.find({
+    select: ['id', 'familyName'],
+  });
+
+  return NextResponse.json(familyData);
+}
